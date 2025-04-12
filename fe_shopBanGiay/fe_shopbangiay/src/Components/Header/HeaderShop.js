b1e121984571cddcde607409bfd103fb4220837a
MaiTrangg <21130573@st.hcmuaf.edu.vn>
2025-04-13 01:17:34 +0700

    add fe_shopBanGiay

---
+++ fe_shopBanGiay/fe_shopbangiay/src/Components/Header/HeaderShop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Topbar from "../HomePage/Topbar";
+import NavbarShop from "./NavbarShop";
+
+const Header = () => {
+    return (
+        <div>
+            <Topbar></Topbar>
+            <NavbarShop></NavbarShop>
+        </div>
+    );
+};
+
+export default Header;

d47c6d06829fb89590a5506e5ab89b71c4cfde15
ngtthtruc <21130582@st.hcmuaf.edu.vn>
2025-05-04 20:35:21 +0700

    api dang ki

---
+++ be_shopBanGiay/be_shopbangiay/src/main/java/com/example/be_shopbangiay/Client/dto/UserDto.java
@@ -0,0 +1,59 @@
+package com.example.be_shopbangiay.Client.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto implements Serializable {
+    private String username;
+    private String email;
+    private String telephone;
+    private String password;
+    private String role;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+}
